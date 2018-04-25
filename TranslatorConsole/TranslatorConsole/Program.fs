b9c53d9e8a7a282e6a97edacdeb898b8a1c91409
Sean Wernimont <swernimont@paylocity.com>
2018-04-25 09:50:02 -0500

    initial commit

---
+++ TranslatorConsole/TranslatorConsole/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Translator
+  
+[<EntryPoint>]
+let main argv =
+    printfn "Enter word to translate: "
+    let word = Console.ReadLine()
+    translateTextAsync word |> Async.RunSynchronously |>  printfn "Translated Word:  %s"    
+    Console.Read() |> ignore
+    0 // return an integer exit code
---
+++ TranslatorConsole/TranslatorConsole/Script1.fsx
@@ -0,0 +1,26 @@
+﻿open System.Net
+open System.IO
+open System.Xml
+
+let translateTextAsync (word:string) =
+    async {
+        try
+           let url = "https://api.microsofttranslator.com/V2/Http.svc/Translate?to=fr-fr&text=" + word;
+           let key = "e8c7cdbe800b4b1faa502ebfd039e977"
+           let request = WebRequest.Create(url)
+           request.Headers.Add("Ocp-Apim-Subscription-Key", key)
+           request.Method <- "GET"
+           let response = request.GetResponseAsync()
+           use reader = new StreamReader(response.Result.GetResponseStream())
+           let output = reader.ReadToEnd()
+           let xml = new XmlDocument()
+           xml.LoadXml(output);
+           reader.Close()
+           response.Result.Close()
+           request.Abort()
+           return xml.FirstChild.InnerText
+        with
+            | ex -> return "Error while trying to translate " + word + ". Exception Message: " + ex.Message
+    }
+
+translateTextAsync "hello" |> Async.RunSynchronously |> printfn "Translation: %s"
