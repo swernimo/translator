6438da0eb9839d125dd0e64a352ff350981d00d4
swernimo <wernimont86@hotmail.com>
2018-04-26 08:32:07 -0500

    checking if file exists.

--- TranslatorConsole/TranslatorConsole/FileManager.fs
+++ TranslatorConsole/TranslatorConsole/FileManager.fs
@@ -4,10 +4,14 @@ open System.IO;
 open Newtonsoft.Json
 open System.Collections.Generic
 
-let loadDictionaryFromJsonFile (filePath:string) =
-    use fileStream = new StreamReader(filePath)
-    let jsonReader = new JsonTextReader(fileStream)
-    let jsonSerialize = new Newtonsoft.Json.JsonSerializer()
-    let dictionary = jsonSerialize.Deserialize<Dictionary<string,string>>(jsonReader)
-    jsonReader.Close()
-    dictionary
+let loadDictionaryFromJsonFile filePath =
+    let fileInfo = new FileInfo(filePath)
+    match fileInfo.Exists with
+    | true -> 
+        use fileStream = new StreamReader(filePath)
+        let jsonReader = new JsonTextReader(fileStream)
+        let jsonSerialize = new Newtonsoft.Json.JsonSerializer()
+        let dictionary = jsonSerialize.Deserialize<Dictionary<string,string>>(jsonReader)
+        jsonReader.Close()
+        dictionary
+    | false -> new Dictionary<string, string>()
