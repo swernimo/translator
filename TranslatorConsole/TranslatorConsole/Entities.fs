d32e85550cb2e6881afa27166cecfb482903ecbe
swernimo <wernimont86@hotmail.com>
2018-04-26 18:15:22 -0500

    added command line parser.

--- TranslatorConsole/TranslatorConsole/Entities.fs
+++ TranslatorConsole/TranslatorConsole/Entities.fs
@@ -1,8 +1,16 @@
 ﻿module Entities
 
 open System.Collections.Generic
+open CommandLine
 
 type Document = {
     Language:string
     Messages: IDictionary<string,string>
+}
+
+ type CommandOptions = {
+    [<Option("sourcePath", Required = true, HelpText = "Path to source file")>] filePath: string;
+    [<Option("languages", Required = true, HelpText = "Comma seperated list of languages to translate to")>] languages: seq<string>;
+    [<Option("key", Required = true, HelpText = "Subscription key for Azure Cognitive Services")>] key: string;
+    [<Option("destination", Required = false, HelpText = "Optional. Location on disk to save the translated file.")>] destination:string option;
 }
--- TranslatorConsole/TranslatorConsole/Program.fs
+++ TranslatorConsole/TranslatorConsole/Program.fs
@@ -1,24 +1,15 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
-open Translator
+﻿open System
 open FileManager
-//output to the same directory as source
-  (*
-    required parameters:
-        source file path
-        languages
-        subscription key
-    optional parameters:
-        destionation file path (use same directory as source file if not supplied)
-  *)
+open Entities
+open CommandLine
 
 [<EntryPoint>]
 let main argv =
-    printf "Enter language to translate to: "
-    let destinationLanguage = Console.ReadLine()
-    //translateJsonDocument "c:\users\swernimont\desktop\messages.en-us.json" destinationLanguage "e8c7cdbe800b4b1faa502ebfd039e977" // |> saveJsonToFile
-    
-    printfn "Press Enter to exit"
-    Console.Read() |> ignore
-    0 // return an integer exit code
+    let result = Parser.Default.ParseArguments<CommandOptions>(argv)
+    match result with
+    | :? Parsed<CommandOptions> as parsed -> 
+        let options = parsed.Value
+        
+        0        
+    | :? NotParsed<CommandOptions> as error -> -1    
+        //translateJsonDocument "c:\users\swernimont\desktop\messages.en-us.json" destinationLanguage "e8c7cdbe800b4b1faa502ebfd039e977" // |> saveJsonToFile
