2b2084dbef004d25f34442600e4e24563bf072e8
Sean Wernimont <swernimont@paylocity.com>
2018-12-09 15:22:00 -0600

    deleting unneeded code.

--- TranslatorConsole/TranslatorConsole/Translator.fs
+++ TranslatorConsole/TranslatorConsole/Translator.fs
@@ -7,50 +7,6 @@ open System.Text.RegularExpressions
 open System.Text
 open Newtonsoft.Json
 open Entities
-(*
-let translateJsonDocument filePath subscriptionKey sourceLanguage destinationLanguage (destinationFilePath:string) =
-    
-    let getEndingString (line:string) =
-        let trimmed = line.Trim()
-        match trimmed.Length with
-        | 0 ->
-            String.Empty
-        | 1 | 2 ->
-            trimmed
-        | _ ->
-            trimmed.Chars(trimmed.Length - 1).ToString()            
-
-    let translateFunc = translateTextAsync destinationLanguage subscriptionKey sourceLanguage
-    let fileInfo = new FileInfo(filePath)
-    match fileInfo.Exists with
-    | true -> 
-        use writer = new StreamWriter(destinationFilePath);
-        File.ReadAllLines(filePath) |> Seq.iter(fun (l:string) -> 
-            let line = l.Replace("\\", String.Empty).Replace("\"", String.Empty).Trim()
-            let ending = getEndingString line
-            match ending with
-            | "{" | "}" | "}," ->
-                writer.WriteLine(l)
-            | "," -> 
-                let split = line.Split(':')
-                let word = split.[1].TrimEnd(',')
-                let translated = translateFunc word |> Async.RunSynchronously
-                let newLine = String.Format("\"{0}\":\"{1}\",", split.[0], translated.Trim())
-                writer.WriteLine(newLine)
-            | _ ->
-                match Regex.Match(ending, "[A-Za-z]").Success with
-                | true ->
-                    let split = line.Split(':')
-                    let word = split.[1].TrimEnd(',')
-                    let translated = translateFunc word |> Async.RunSynchronously
-                    let newLine = String.Format("\"{0}\":\"{1}\"", split.[0], translated.Trim())
-                    writer.WriteLine(newLine)
-                | false ->
-                    //ends in a number or special character don't translate
-                    writer.WriteLine(l)
-        )
-        ()
-        *)
 
 let getTranslations sourceLanguage destinationLanguages subscriptionKey wordsToTranslate =
     let buildRequestBody (wordsToTranslate:string[]) =
